07356c597fc2ea80d9e12b5694f9c0bcdeb03804
brycekelleher <bryce_kelleher@hotmail.com>
2016-06-14 17:57:48 +1000

    Added a function to setup a basic texture in GL

--- glutils.cpp
+++ glutils.cpp
@@ -35,6 +35,17 @@ static void DrawTexturedQuad()
 	glEnd();
 }
 
+static void MakeTexture(int imagew, int imageh, void *pixels)
+{
+	glGenTextures(1, texobj);
+	glBindTexture(GL_TEXTURE_2D, texobj[0]);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imagew, imageh, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+}
+
 // draw a 3d wireframe bounding box
 static void DrawBounds(float min[3], float max[3])
 {
